1da77eff718d299e64b4cef8e5f12feb5d895d89
AaronDjc <aaron_djc@foxmail.com>
2020-05-23 20:35:59 +0800

    new

---
+++ design-principle/src/test/java/com/aaron/designprinciple/DesignPrincipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaron.designprinciple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPrincipleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
